d0c6e5fee2e5d62352b1e89cf6ca0b756791941c
Sphitik <h_dafare@rediffmail.com>
2025-10-31 12:56:22 +0000

    Added text prompt to use CLI directly

---
+++ main.py
@@ -0,0 +1,27 @@
+import os, sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
+if len(sys.argv)<2:
+    sys.exit(1)
+
+query=" ".join(sys.argv[1:])
+
+messages = [
+    types.Content(role="user", parts=[types.Part(text=query)]),
+]
+
+
+response = client.models.generate_content(
+    model='gemini-2.0-flash-001', contents=messages
+)
+
+print(response.text)
+# print(response.usage_metadata.cache_tokens_details)
+print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
+print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
