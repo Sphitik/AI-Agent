13d00dea3422ad23350ebc5b0e225df36baa4487
Sphitik <h_dafare@rediffmail.com>
2025-10-31 13:28:20 +0000

    updated verbose implementation

--- main.py
+++ main.py
@@ -3,25 +3,33 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
+def main():
 
-if len(sys.argv)<2:
-    sys.exit(1)
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
-query=" ".join(sys.argv[1:])
+    if len(sys.argv)<2:
+        sys.exit(1)
 
-messages = [
-    types.Content(role="user", parts=[types.Part(text=query)]),
-]
+    query=" ".join(sys.argv[1:3])
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=query)]),
+    ]
 
 
-response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents=messages
-)
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', contents=messages
+    )
 
-print(response.text)
-# print(response.usage_metadata.cache_tokens_details)
-print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
-print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
+    print(response.text)
+    # print(response.usage_metadata.cache_tokens_details)
+
+    if len(sys.argv)==3 and sys.argv[2]=='--verbose':
+        print(f'User prompt: {query}')
+        print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
+        print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
+
+if __name__=="__main__":
+    main()
